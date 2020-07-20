5aeb09bb6fb5e06f4960600cf78d11666cd9e718
Alok Gandhi <alok.gandhi2002@gmail.com>
2020-07-20 08:31:11 +0530

    Added cell flagging logic

--- src/minescrubber_core/abstract.py
+++ src/minescrubber_core/abstract.py
@@ -11,6 +11,10 @@ class UI(abc.ABC):
         ...
 
     @abc.abstractmethod
+    def game_solved(self):
+        ...
+
+    @abc.abstractmethod
     def run(self):
         ...
 
@@ -19,7 +23,15 @@ class UI(abc.ABC):
         ...
 
     @abc.abstractproperty
+    def cell_flagged_signal(self):
+        ...
+
+    @abc.abstractproperty
     def new_game_signal(self):
+        ...
+
+    @abc.abstractproperty
+    def wiring_method_name(self):
         ...
 
 
@@ -36,13 +48,24 @@ class Controller:
         board = boarder.Board()
         ui = ui_class(board=board)
         game = gamer.Game(board=board, ui=ui)
-        ui.cell_selected_signal.connect(game.select)
-        ui.new_game_signal.connect(game.reset)
+
+        self._do_wiring(ui, game)
         ui.run()
 
         post_args = post_args or []
         post_kwargs = post_kwargs or {}
         self.post_callback(*post_args, **post_kwargs)
+
+    @staticmethod
+    def _do_wiring(ui, game):
+        wiring = {
+            ui.cell_selected_signal: game.select,
+            ui.cell_flagged_signal: game.flag,
+            ui.new_game_signal: game.reset
+        }
+        for connect_from, connect_to in wiring.items():
+            wiring_method = getattr(connect_from, ui.wiring_method_name)
+            wiring_method(connect_to)
 
     @abc.abstractmethod
     def pre_callback(self, *args, **kwargs):
